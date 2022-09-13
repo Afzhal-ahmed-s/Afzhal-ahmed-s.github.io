4cba60acaf08052c11abb4d368e8345257284dda
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-09-13 12:48:44 +0530

    In the making
    Need to insert project section

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // document.getElementById("box2").onclick = function() {
 //     location.href = "https://drive.google.com/file/d/1AmW1jNFOp7_1hQYu2CGv8I2kMujleaa7/view?usp=sharing";
 // };
+
+document.getElementsByTagName("a").onclick = function() {
+    document.getElementsByTagName("a").style.textDecoration = "underline !important";
+};
