9f623adf4d83faf17697b6971398cf3856a6bc62
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-09-15 21:08:24 +0530

    Added github calendar and done some polishing

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// document.getElementById("box2").onclick = function() {
-//     location.href = "https://drive.google.com/file/d/1AmW1jNFOp7_1hQYu2CGv8I2kMujleaa7/view?usp=sharing";
-// };
-
 document.getElementsByTagName("a").onclick = function() {
     document.getElementsByTagName("a").style.textDecoration = "underline !important";
 };
