06b6eed16bbe7239209c61f8372e0a5660ef8553
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-09-19 00:03:16 +0530

    Animations added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementsByTagName("a").onclick = function() {
+document.getElementsByTagName("a").onhover = function() {
     document.getElementsByTagName("a").style.textDecoration = "underline !important";
+};
+
+document.getElementById("box2").onhover = function() {
+    console.log("Hovering box");
 };
