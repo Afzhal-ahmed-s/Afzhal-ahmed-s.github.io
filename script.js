92b63205452e2bc23009a03a2e03a6d07eaefa8a
Afzhal-ahmed-s <96117548+Afzhal-ahmed-s@users.noreply.github.com>
2022-09-19 18:24:57 +0530

    Portfolio V1

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ document.getElementsByTagName("a").onhover = function() {
 document.getElementById("box2").onhover = function() {
     console.log("Hovering box");
 };
+
+hideOrShowTechBox();
+
+
+window.addEventListener("resize", function() {
+   hideOrShowTechBox();
+});
+
+function hideOrShowTechBox() {
+    if(screen.width > 480){
+        document.getElementById("techBoxDesktop").style.display = "block";
+        document.getElementById("techBoxMobile").style.display = "none";
+    }
+    else{
+        document.getElementById("techBoxMobile").style.display = "block";
+        document.getElementById("techBoxDesktop").style.display = "none";
+    }
+}
